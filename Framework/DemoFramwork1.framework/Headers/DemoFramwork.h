38ae9f7cfa84e737c11958c04c957782f3dc3a6d
张鹤 <15169274561@163.com>
2020-12-21 11:10:11 +0800

    提交

---
+++ Framework/DemoFramwork1.framework/Headers/DemoFramwork.h
@@ -0,0 +1,18 @@
+//
+//  DemoFramwork.h
+//  DemoFramwork
+//
+//  Created by 张鹤 on 2020/12/18.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DemoFramwork.
+FOUNDATION_EXPORT double DemoFramworkVersionNumber;
+
+//! Project version string for DemoFramwork.
+FOUNDATION_EXPORT const unsigned char DemoFramworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoFramwork/PublicHeader.h>
+
+
